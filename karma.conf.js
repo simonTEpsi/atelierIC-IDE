b1bad0b34e618d00b7785b9ac95e4cd58e9d3362
Tassaert <simon.tassaert@epsi.fr>
2020-03-26 11:13:09 +0100

    update .travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -25,12 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["ChromeHeadlessNoSandbox"],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    singleRun: false,
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: "ChromeHeadless",
         flags: ["--no-sandbox"]
       }
     },
-
+  });
 };
